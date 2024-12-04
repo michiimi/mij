725aee90cdac93e8aabddc021cc191ee602c891e
michiimi <mivhujojil@gmail.com>
2024-12-04 10:43:18 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+cuteBot.trackEvent(cuteBot.MbPins.Left, cuteBot.MbEvents.FindLine, function () {
+    cuteBot.turnleft()
+    cuteBot.forward()
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Blues), music.PlaybackMode.InBackground)
+})
+let _30: neopixel.Strip = null
+let range = _30.range(3, 4)
+cuteBot.moveTime(cuteBot.Direction.forward, neopixel.rgb(255, 255, 255), 20)
+loops.everyInterval(500, function () {
+	
+})
+basic.forever(function () {
+	
+})
